fc61e6a90baf05b492c19c6cdfad2f734e769174
Tran Danh Trong <codevnes@gmail.com>
2025-06-08 21:42:39 +0700

    Update Sun Jun  8 21:42:39 +07 2025

--- dist/config/auth.config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.authConfig = void 0;
-exports.authConfig = {
-    jwtSecret: process.env.JWT_SECRET || 'vsmi-secret-key',
-    jwtExpiration: 86400, // 24 hours in seconds
-    jwtRefreshExpiration: 604800, // 7 days in seconds
-};
