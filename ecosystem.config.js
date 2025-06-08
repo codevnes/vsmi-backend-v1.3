1d38bb360c926d491776a3f28d5efefc26edb846
Tran Danh Trong <codevnes@gmail.com>
2025-06-08 21:28:22 +0700

    Update Sun Jun  8 21:28:22 +07 2025

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: 'vsmi-backend-v2',
       script: './dist/index.js',
-      instances: 1,
+      instances: 'max',
       exec_mode: 'cluster',
       watch: false,
       env: {
@@ -17,7 +17,13 @@ module.exports = {
       max_memory_restart: '1G',
       autorestart: true,
       merge_logs: true,
-      log_date_format: 'YYYY-MM-DD HH:mm:ss'
+      log_date_format: 'YYYY-MM-DD HH:mm:ss',
+      node_args: '--max-old-space-size=1024',
+      kill_timeout: 3000,
+      wait_ready: true,
+      listen_timeout: 50000,
+      cron_restart: '0 3 * * *',
+      exp_backoff_restart_delay: 100
     }
   ]
 }; 
